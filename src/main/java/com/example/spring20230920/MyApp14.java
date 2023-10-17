8040168d4722fb806bb49887f5ba58520e7cd3a3
hyeon <731aa@naver.com>
2023-10-17 17:24:44 +0900

    10172023

--- src/main/java/com/example/spring20230920/MyApp14.java
+++ src/main/java/com/example/spring20230920/MyApp14.java
@@ -17,6 +17,23 @@ public class MyApp14 {
 
         Object bean2 =context.getBean("myBean2");
         System.out.println("bean2 = " + bean2);
+
+        Configuration1 conf = context.getBean("Configuration1", Configuration1.class);
+        MyClass27 obj1 = conf.myBean1();
+        MyClass27 obj2 = conf.myBean2();
+
+        System.out.println("obj1 = " + obj1);
+        System.out.println("obj2 = " + obj2);
+
+        System.out.println(conf.myBean1());
+        System.out.println(conf.myBean2());
+        System.out.println(new MyClass27());
+        System.out.println(new MyClass27());
+//어플리케이션 내에서 어떤 클래스의 객체가((인스턴스가 하나가되도록 하는 ) == single ton pattern
+//        application 안의 클래스가 객체가 메소드를 재정의함으로써 인스턴스가 하나가되도록 한다
+//        configuration1의 싱글톤 패턴으로 재정의함. 재정의한 메소드를 호출한 것이므로 호출할 때마다 항상 같은 객체가 호출된다.
+//재정의 . 항상 같은 객체의 출력, 어플리케이션 내에서 어떤 클래스의 instance가 하나가 되도록 함.
+//        singleton pattern
     }
 }
 @Configuration
@@ -27,14 +44,19 @@ class Configuration1{
     //bean의 이름은 메소드 명과 같다.
 
     @Bean
-    public MyClass27 myBeab1(){
+    public MyClass27 myBean1(){
         return new MyClass27();
     }
     @Bean
-    public MyClass27 myBeab2(){
+    public MyClass27 myBean2(){
         return new MyClass27();
     }
 }
 //@configuration을 붙였을 때 빈의 이름이 클래스 이름을 lowerCameCase로 바꾼것,
 @Configuration
 class MyClass27{}
+
+
+
+//재정의돤 메소드
+//왜 재정의를 했는지 같은 객체가 나오는걸 보장하도록 메소드가 다시
