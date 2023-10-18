ebb4d4a616a55d381f019c276850d1f3076069d5
hyeon <731aa@naver.com>
2023-10-18 10:33:15 +0900

    20231018

---
+++ src/main/java/com/example/spring20230920/Controller/Controller29.java
@@ -0,0 +1,82 @@
+package com.example.spring20230920.Controller;
+
+import com.example.spring20230920.dao.MyDao3;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("main29")
+public class Controller29 {
+
+    private final MyDao3 dao3;
+
+    @GetMapping("sub1")
+    public void method1() {
+        String name = dao3.select1();
+        System.out.println("name = " + name);
+    }
+
+    @GetMapping("sub2")
+    public void method2() {
+        Double price = dao3.select2();
+
+        System.out.println("price = " + price);
+    }
+
+    @GetMapping("sub3")
+    public void method3() {
+        // 1번 직원의 LastName 조회 후 출력
+        String name = dao3.select3();
+        System.out.println("name = " + name);
+    }
+
+    @GetMapping("sub4")
+    public void method4() {
+        // 2번 직원의 생일 조회 후 출력
+        LocalDate birth = dao3.select4();
+        System.out.println("birth = " + birth);
+    }
+
+    @GetMapping("sub5")
+    public void method5() {
+        List<String> countrys = dao3.select5();
+
+        countrys.forEach(System.out::println);
+    }
+
+    @GetMapping("sub6")
+    public void method6() {
+        // 직원들의 생일 조회
+        List<LocalDate> births = dao3.select6();
+        System.out.println("births = " + births.size());
+    }
+
+    @GetMapping("sub7")
+    public void method7() {
+        // 상품의 가격들 조회
+        List<Double> priceList = dao3.select7();
+        System.out.println("priceList = " + priceList.size());
+    }
+    @GetMapping("sub8")
+    public void method8() {
+        // 상품의 가격들 조회
+        Map<String, Object> row = dao3.select8();
+        System.out.println("row = " + row);
+    //map에 keyvalue entry는 순서가 보장되지 않는다.
+    }
+    @GetMapping("sub9")
+    public void method9() {
+        // 상품명, 가격, 카테고리 명 조회
+        Map<String, Object> map = dao3.select9();
+        map.entrySet().forEach(System.out::println);
+        }
+
+}
---
+++ src/main/java/com/example/spring20230920/dao/MyDao3.java
@@ -0,0 +1,68 @@
+package com.example.spring20230920.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface MyDao3 {
+
+
+    // 1행 1열
+    @Select("""
+            SELECT customerName FROM customers
+            WHERE CustomerID = 1
+            """)
+    String select1();
+
+    @Select("""
+            SELECT price FROM products
+            WHERE ProductID = 1
+            """)
+    Double select2();
+
+    @Select("""
+        SELECT LastName FROM employees
+        WHERE EmployeeID = 1
+        """)
+    String select3();
+
+    @Select("""
+            SELECT BirthDate FROM employees
+            WHERE EmployeeID = 2
+            """)
+    LocalDate select4();
+
+    @Select("""
+            SELECT DISTINCT Country FROM customers
+            ORDER BY 1 
+            """)
+    List<String> select5();
+
+    @Select("""
+        SELECT BirthDate FROM employees
+        """)
+    List<LocalDate> select6();
+
+    @Select("""
+        SELECT Price FROM products
+        """)
+    List<Double> select7();
+
+    @Select("""
+            SELECT CustomerID, CustomerName, Address 
+            FROM customers
+            WHERE CustomerID = 1
+            """)
+    Map<String, Object> select8();
+
+    @Select("""
+    SELECT ProductName, Price, CategoryName
+    FROM products p JOIN categories c ON p.CategoryID = c.CategoryID
+    WHERE p.ProductID = 1
+    """)
+    Map<String, Object> select9();
+}
