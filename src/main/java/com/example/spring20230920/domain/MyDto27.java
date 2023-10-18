9f6d5245cca1732bba12ad58d939745c5c382c25
hyeon <731aa@naver.com>
2023-10-18 17:45:08 +0900

    20231018

---
+++ src/main/java/com/example/spring20230920/domain/MyDto27.java
@@ -0,0 +1,7 @@
+package com.example.spring20230920.domain;
+
+import java.time.LocalDate;
+
+public class MyDto27 {
+    private LocalDate from;
+}
---
+++ src/main/java/com/example/spring20230920/domain/MyDto28.java
@@ -0,0 +1,10 @@
+package com.example.spring20230920.domain;
+
+import lombok.Data;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+@Data
+public class MyDto28 {
+    private LocalDate to;
+}
