f561f263b5c762100e806774f64bbf54f70a20ab
hyeon <731aa@naver.com>
2023-10-19 19:58:27 +0900

    20231019

---
+++ sql/Afterlecture1.sql
@@ -0,0 +1,24 @@
+CREATE TABLE PRACTICE2(
+                          Artist VARCHAR(100),
+                          Album_Title VARCHAR(100),
+                          Genre VARCHAR(100),
+                          FORM VARCHAR(100),
+                          Release_Date VARCHAR(100),
+                          Price VARCHAR(100)
+);
+INSERT INTO practice2(Artist, Album_Title, Genre, FORM, Release_Date, Price)
+VALUES('Mitski','BeTheCowboy','Indie Rock','CD','2018-08-17','23,300₩'),
+      ('Mitski','','','CD','','₩'),
+      ('','','','CD','','₩'),
+      ('','','','CD','','₩'),
+      ('','','','CD','','₩'),
+      ('','','','CD','','₩'),
+      ('','','','CD','','₩'),
+      ('Mitski','','','CD','','₩'),
+      ('','','','CD','','₩'),
+      ('','','','CD','','₩'),
+      ('','','','CD','','₩'),
+      ('','','','CD','','₩'),
+      ('','','','CD','','₩'),
+      ('','','','CD','','₩')
+
