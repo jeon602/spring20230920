c07ff6cd4d20e9e9c50af632ee61a1a391c2b10d
jeon602 <143367041+jeon602@users.noreply.github.com>
2023-10-19 20:10:06 +0900

    20231019 8:09

--- sql/Afterlecture1.sql
+++ sql/Afterlecture1.sql
@@ -8,7 +8,23 @@ CREATE TABLE PRACTICE2(
 );
 INSERT INTO practice2(Artist, Album_Title, Genre, FORM, Release_Date, Price)
 VALUES('Mitski','BeTheCowboy','Indie Rock','CD','2018-08-17','23,300₩'),
-      ('Mitski','','','CD','','₩'),
+      ('MINSU', 'XXLOVE','Indie Rock','CD', '2019-10-03','14,900₩')
+ ('J.UNA', 'DISCOVERY', 'Indie Rock','CD','2021-02-10','14,900₩')
+ ('SOLE', 'INMAGIN_CLUB', 'Indie Rock','CD','2019-08-08','14,900₩')
+ ('ASHE', 'ASHLYN','Indie Rock','CD', '2021-05-07','14,900₩')
+ ('ASHE', 'MOEALOFTHESTORY','Indie Rock', 'CD','2019-08-09','14,900₩')
+ ('AJR', 'BANG!','Indie Rock','CD', '2020-02-12','14,900₩')
+('STELLA.JANJ', 'COLORS', 'Indie Rock','CD','2016-10-06','14,900₩')
+ ('JUNGWOO', '6TH SATURDAY', 'Indie Rock','CD','2019-09-05','14,900₩')
+ ('SORAN', 'AAA','Indie Rock', 'CD','2017-11-22','14,900₩')
+ ('THEWEEKND', 'THR_HIGHLIGHT','Indie Rock','CD', '2021-02-05','14,900₩')
+('ANTS', 'ANTS_SAY','Indie Rock', 'CD','2016-10-07','14,900₩')
+('CULTS', 'ALWAYS_FOREVER', 'Indie Rock','CD','2013-10-11','14,900₩')
+
+
+
+
+('Mitski','','','CD','','₩'),
       ('','','','CD','','₩'),
       ('','','','CD','','₩'),
       ('','','','CD','','₩'),
